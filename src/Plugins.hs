50288f6b38162428827734caaa2a448663801f15
Ömer Sinan Ağacan <omeragacan@gmail.com>
2015-10-27 11:18:10 -0400

    implement evalUnlines

--- src/Plugins.hs
+++ src/Plugins.hs
@@ -1,19 +1,22 @@
-{-# LANGUAGE CPP, LambdaCase, ScopedTypeVariables #-}
+{-# LANGUAGE CPP, LambdaCase, ScopedTypeVariables, TupleSections #-}
 
 module Plugins (plugin) where
 
 --------------------------------------------------------------------------------
 
-import           Class         (classMethods)
-import           CoreLint      (lintPassResult)
+import           Class                 (classMethods)
+import           CoreLint              (lintPassResult)
 import           GhcPlugins
-import           InstEnv       (instanceDFunId)
-import           PrelNames     (numClassName)
-import           TcEnv         (tcLookupClass, tcLookupInstance)
-import           TcRnMonad     (initTcForLookup)
-import           TysPrim       (intPrimTy)
+import           InstEnv               (instanceDFunId)
+import           PrelNames             (numClassName)
+import           TcEnv                 (tcLookupClass, tcLookupInstance)
+import           TcRnMonad             (initTcForLookup)
+import           TysPrim               (intPrimTy)
 
 import           Control.Monad
+
+import qualified Data.ByteString       as BS
+import qualified Data.ByteString.Char8 as BSC
 
 --------------------------------------------------------------------------------
 
@@ -24,15 +27,32 @@ plugin = defaultPlugin{installCoreToDos = installPlugin}
     installPlugin _ todos = do
       liftIO $ putStrLn "installing the plugin"
       pprTrace "TODOs:" (ppr todos) (return ())
-      return (CoreDoPluginPass "Implementing functions" implementFuns : todos)
+      return ( CoreDoPluginPass "Implementing functions" implementFuns
+             : CoreDoPluginPass "Evaluate unlines" evalUnlines
+             : todos
+             )
+
+--------------------------------------------------------------------------------
 
 implementFuns :: ModGuts -> CoreM ModGuts
 implementFuns guts@ModGuts{ mg_binds = binds, mg_rdr_env = rdrEnv } = do
-    liftIO $ putStrLn "running the plugin"
+    liftIO $ putStrLn "Implementing functions..."
     binds' <- mapM (implementAddOne rdrEnv >=> implementFac rdrEnv) binds
     hscEnv <- getHscEnv
-    liftIO $ lintPassResult hscEnv (CoreDoPluginPass "Implementing functions" implementFuns) binds'
+    lint (CoreDoPluginPass "Implementing functions" implementFuns) binds'
     return guts{ mg_binds = binds' }
+
+evalUnlines :: ModGuts -> CoreM ModGuts
+evalUnlines guts@ModGuts{ mg_binds = binds, mg_rdr_env = rdrEnv } = do
+    liftIO $ putStrLn "Evaluating unlines..."
+    binds' <- mapM evalUnlines' binds
+    lint (CoreDoPluginPass "Evaluate unlines" evalUnlines) binds'
+    return guts{ mg_binds = binds' }
+
+lint :: CoreToDo -> CoreProgram -> CoreM ()
+lint pass pgm = do
+    hscEnv <- getHscEnv
+    liftIO $ lintPassResult hscEnv pass pgm
 
 --------------------------------------------------------------------------------
 
@@ -161,9 +181,68 @@ implementFac rdrEnv (NonRec bndr _)
 implementFac _ bndr = return bndr
 
 --------------------------------------------------------------------------------
+
+evalUnlines' :: CoreBind -> CoreM CoreBind
+evalUnlines' (NonRec bndr rhs) = NonRec bndr <$> evalUnlinesExpr rhs
+evalUnlines' (Rec bs)          = Rec <$> mapM (\(bndr, rhs) -> (bndr,) <$> evalUnlinesExpr rhs) bs
+
+evalUnlinesExpr :: CoreExpr -> CoreM CoreExpr
+evalUnlinesExpr e@Var{} = return e
+evalUnlinesExpr e@Lit{} = return e
+
+evalUnlinesExpr e@(App (Var v) arg)
+  | occNameString (nameOccName (varName v)) == "unlines"
+  = do liftIO (putStrLn "Found a unlines!")
+       case tryCollectList arg of
+
+         Just listArgs -> do
+           pprTrace "list args:" (ppr listArgs) (return ())
+           case mapM tryExtractString listArgs of
+             Nothing -> return e
+             Just ss -> do
+               pprTrace "strings:" (hsep . map pprHsBytes $ ss) (return ())
+               mkStringExprFS $ mkFastStringByteString $ BS.intercalate (BSC.pack "\n") ss
+
+         Nothing -> do
+           pprTrace "can't parse list args" empty (return e)
+
+evalUnlinesExpr (App e1 e2)    = App <$> evalUnlinesExpr e1 <*> evalUnlinesExpr e2
+evalUnlinesExpr (Lam arg body) = Lam arg <$> evalUnlinesExpr body
+evalUnlinesExpr (Let bs body)  = Let <$> evalUnlines' bs <*> evalUnlinesExpr body
+evalUnlinesExpr (Case scrt bndr ty alts) = do
+    scrt' <- evalUnlinesExpr scrt
+    alts' <- mapM evalUnlinesAlt alts
+    return $ Case scrt' bndr ty alts'
+evalUnlinesExpr (Cast e c) = Cast <$> evalUnlinesExpr e <*> pure c
+evalUnlinesExpr (Tick t e) = Tick t <$> evalUnlinesExpr e
+evalUnlinesExpr e@Type{} = return e
+evalUnlinesExpr e@Coercion{} = return e
+
+evalUnlinesAlt :: CoreAlt -> CoreM CoreAlt
+evalUnlinesAlt = return
+
+--------------------------------------------------------------------------------
 -- * Utils
 
 newLocalId :: String -> Type -> CoreM Id
 newLocalId str ty = do
     uniq <- getUniqueM
     return (mkSysLocal (mkFastString str) uniq ty)
+
+tryCollectList :: CoreExpr -> Maybe [CoreExpr]
+tryCollectList e@App{}
+  | (Var v, [_typeArgument, arg1, arg2]) <- collectArgs e
+  , occNameString (nameOccName (varName v)) == ":"
+  = (arg1 :) <$> tryCollectList arg2
+
+  | (Var v, [_typeArgument]) <- collectArgs e
+  , occNameString (nameOccName (varName v)) == "[]"
+  = Just []
+
+tryCollectList e = Nothing
+
+tryExtractString :: CoreExpr -> Maybe BS.ByteString
+tryExtractString (App (Var v) (Lit (MachStr bs)))
+  | occNameString (nameOccName (varName v)) == "unpackCString#"
+  = Just bs
+tryExtractString _ = Nothing
--- test/Main.hs
+++ test/Main.hs
@@ -11,7 +11,15 @@ addOne = undefined
 factorial :: Int -> Int
 factorial = undefined
 
+printHelp :: IO ()
+printHelp = putStrLn helpMsg
+  where
+    helpMsg = unlines ["This is a help",
+                       "message that takes",
+                       "couple of lines."]
+
 main :: IO ()
 main = do
+    printHelp
     forM_ [1..10] $ print . addOne
     forM_ [1..10] $ print . factorial
